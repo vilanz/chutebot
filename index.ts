45206a228ad3c929d0c6a2ce203f3077b34bca6f
vilanz <vs.lanzarini@gmail.com>
2021-05-24 18:51:30 -0300

    feat: barebones structure

---
+++ index.ts
@@ -0,0 +1,19 @@
+import Discord from 'discord.js';
+
+require('dotenv').config()
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    if (client.user) {
+        console.log(`Logged in as ${client.user.tag}!`);
+    }
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('pong');
+    }
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
