9d6f1d58e62ff18e82024656516df24aeaf7f757
vilanz <vs.lanzarini@gmail.com>
2021-05-24 19:21:18 -0300

    chore: add linting

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,5 @@
+// we're extending process.env so we can bypass "no-unused-vars"
+// eslint-disable-next-line no-unused-vars
 declare namespace NodeJS {
     export interface ProcessEnv {
         DISCORD_BOT_TOKEN: string;
--- index.ts
+++ index.ts
@@ -1,19 +1,20 @@
 import Discord from 'discord.js';
+import dotenv from 'dotenv';
 
-require('dotenv').config()
+dotenv.config();
 
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    if (client.user) {
-        console.log(`Logged in as ${client.user.tag}!`);
-    }
+  if (client.user) {
+    console.log(`Logged in as ${client.user.tag}!`);
+  }
 });
 
-client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('pong');
-    }
+client.on('message', (msg) => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
