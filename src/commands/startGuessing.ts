58cac2f78f9ca389bec1d77e7439a781309f33e7
vilanz <vs.lanzarini@gmail.com>
2021-05-25 00:59:09 -0300

    feat: add barebones guess impl

--- src/commands/startGuessing.ts
+++ src/commands/startGuessing.ts
@@ -1,13 +1,24 @@
 import Discord from 'discord.js';
+import { parseCommand } from './parser';
 import { CommandHandler } from './types';
 
-const playerGuessFilter = (message: Discord.Message) => message.content.startsWith('g!g') && message.content === 'g!josé';
+const playerGuessFilter = (name: string) => (message: Discord.Message) => {
+  const command = parseCommand(message.content);
+  if (!command || command.name !== 'g') {
+    return false;
+  }
+  return command.args === name;
+};
 
 export const startGuessing: CommandHandler = async (command, message) => {
   message.channel.send('Iniciando quiz...');
-  message.channel.awaitMessages(playerGuessFilter, { max: 1, time: 30000, errors: ['time'] })
+
+  // TODO use a random player from a real database
+  const playerName = command.args;
+
+  message.channel.awaitMessages(playerGuessFilter(playerName), { max: 1, time: 10000, errors: ['time'] })
     .then((collected) => {
-      message.channel.send(`${collected.first()!.author} acertou!`);
+      message.channel.send(`${collected.first()!.author} acertou! Era o ${playerName}.`);
     })
     .catch(() => {
       message.channel.send('Ninguém acertou depois de 10 segundos :(');
