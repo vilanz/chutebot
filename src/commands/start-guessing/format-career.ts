c1513c8b98dc674d483dd66b331558e70d032f54
vilanz <vs.lanzarini@gmail.com>
2021-06-22 22:50:09 -0300

    formatting club

--- src/commands/start-guessing/format-career.ts
+++ src/commands/start-guessing/format-career.ts
@@ -2,8 +2,31 @@ import { flag } from "country-emoji";
 import { MessageEmbed } from "discord.js";
 import { PlayerSpell, PlayerSpellClub } from "../../data";
 
-const formatClub = (club?: PlayerSpellClub): string =>
-  club ? `${flag(club.country)} ${club.name}` : "Sem clube";
+const formatTransferFee = (transferFee: string): string => {
+  switch (transferFee) {
+    case "free transfer":
+      return "de graça";
+    case "loan transfer":
+      return "emprestado";
+    case "End of loan":
+      return "retornando de empréstimo";
+    case "-":
+      return "arregou";
+    case "?":
+      return "desconhecido";
+    default:
+      return transferFee;
+  }
+};
+
+const formatClub = (club?: PlayerSpellClub): string => {
+  if (!club) {
+    return "Sem clube";
+  }
+  // England has no ISO emoji flag
+  const country = club.country === "England" ? "UK" : club.country;
+  return `${flag(country)} ${club.name}`;
+};
 
 export const getPlayerSpellsEmbed = (spells: PlayerSpell[]): MessageEmbed =>
   new MessageEmbed()
@@ -13,7 +36,9 @@ export const getPlayerSpellsEmbed = (spells: PlayerSpell[]): MessageEmbed =>
         value: [
           `:point_right: ${formatClub(spell.to)}`,
           `:point_left: ${formatClub(spell.from)}`,
-          `Por ${spell.transferFee} em ${spell.date.toLocaleDateString()}`,
+          `${spell.date.toLocaleDateString()}: ${formatTransferFee(
+            spell.transferFee
+          )}`,
         ].join("\n"),
       }))
     )
