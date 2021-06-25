69978f175e9a24f44efb5c8589024a6110f3e303
vilanz <vs.lanzarini@gmail.com>
2021-06-25 00:38:31 -0300

    adding back syncDatabase

--- src/data/db/instance.ts
+++ src/data/db/instance.ts
@@ -1,4 +1,10 @@
 // eslint-disable-next-line max-classes-per-file
 import { Sequelize } from "sequelize";
+import { PlayerEntity, PlayerSpellEntity } from "./models";
 
 export const sequelizeInstance = new Sequelize("sqlite::memory:");
+
+export const syncDatabase = async () => {
+  await PlayerEntity.sync();
+  await PlayerSpellEntity.sync();
+};
