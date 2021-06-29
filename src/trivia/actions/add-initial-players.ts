983d483f3281006052584ca9357c2feee412ade4
vilanz <vs.lanzarini@gmail.com>
2021-06-29 20:55:19 -0300

    adding more players

--- src/trivia/actions/add-initial-players.ts
+++ src/trivia/actions/add-initial-players.ts
@@ -4,6 +4,18 @@ const INITIAL_PLAYER_IDS = [
   423839, // Ribamar,
   68290, // Neymar,
   265660, // Yago Pikachu,
+  342229, // Mbappé
+  169052, // Edenilson
+  191614, // Fred
+  38253, // Lewandowski
+  418560, // Haaland
+  96341, // Lukaku
+  105521, // Immobile
+  148455, // Salah
+  240306, // Bruno Fernandes
+  132098, // Harry Kane
+  225693, // Fabinho
+  74605, // Walter
 ];
 
 export const addInitialTriviaPlayers = async (): Promise<void> => {
