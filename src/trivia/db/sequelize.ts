cc8d7c956c90a72be5143a3095f10545319a32e3
vilanz <vs.lanzarini@gmail.com>
2021-07-05 23:11:46 -0300

    removing sqlite logging

--- src/trivia/db/sequelize.ts
+++ src/trivia/db/sequelize.ts
@@ -1,9 +1,8 @@
 import path from "path";
 import { Sequelize } from "sequelize";
-import { logger } from "../../core/log";
 
 export const sequelizeInstance = new Sequelize({
   dialect: "sqlite",
   storage: path.resolve("./db.sqlite"),
-  logging: (msg) => logger.info(msg),
+  logging: false,
 });
