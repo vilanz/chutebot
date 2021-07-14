ca82bb4f146bfc144fda9a9154b3c247593c6373
vilanz <vs.lanzarini@gmail.com>
2021-07-13 21:24:25 -0300

    revamp help command

--- src/trivia/commands/help.ts
+++ src/trivia/commands/help.ts
@@ -1,44 +1,26 @@
-import { stripIndents } from "common-tags";
+import { MessageEmbed } from "discord.js";
 import { CommandHandler } from "../../core/command-parser";
 
-const NOTES = stripIndents`
-- Caso ocorrer um erro ao executar um comando, o bot reagirá com ⚠ à mensagem que invocou o comando.
-- A postagem de gols do Twitter nos canais é controlada via comandos admin.
-- Código fonte: https://github.com/vilanz/chutebot
-`;
-
 export const help: CommandHandler = async (message) => {
+  const TRIVIA_EMBED = new MessageEmbed()
+    .setTitle("Comandos de trivia")
+    .addField("c!start", "Inicia uma sessão de trivia que durará 25 segundos.")
+    .addField(
+      "c!g <jogador>",
+      "Adivinhar o nome do jogador na sessão de trivia atual."
+    )
+    .addField("c!wins", "High scores do trivia.")
+    .addField("c!add <pesquisa>", "Adicionar jogadores ao bot.");
+  const GENERAL_EMBED = new MessageEmbed()
+    .setTitle("Comandos gerais")
+    .addField("c!help", "Mostra esse guia.")
+    .addField("c!ping", "Latência do bot.");
+  const NOTES_EMBED = new MessageEmbed()
+    .setTitle("Notas")
+    .addField("Erro ao executar comando! :(", "O bot reagirá ao comando com ⚠.")
+    .addField("Postagem de gols do Twitter", "Controlados via comandos admin.")
+    .addField("Código fonte", "https://github.com/vilanz/chutebot");
   await message.reply({
-    embeds: [{
-      title: 'Comandos',
-      fields: [{
-        name: 'c!start',
-        value: 'Inicia uma sessão de trivia que durará 25 segundos.',
-        inline: true
-      }, {
-        name: 'c!g <jogador>',
-        value: 'Adivinhar o nome do jogador na sessão de trivia atual.',
-        inline: true
-      }, {
-        name: 'c!add <pesquisa>',
-        value: 'Escolher um de até 5 jogadores para serem adicionados ao bot.',
-        inline: true
-      }, {
-        name: 'c!wins',
-        value: 'High scores do trivia.',
-        inline: true
-      }, {
-        name: 'c!help',
-        value: 'Mostra esse guia.',
-        inline: true
-      }, {
-        name: 'c!ping',
-        value: 'Latência do bot.',
-        inline: true
-      }, {
-        name: 'Notas',
-        value: NOTES
-      }]
-    }]
+    embeds: [TRIVIA_EMBED, GENERAL_EMBED, NOTES_EMBED],
   });
 };
