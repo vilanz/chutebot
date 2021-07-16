f94fc32e18034e38b50f5b1170c8d9d776e0ade6
vilanz <vs.lanzarini@gmail.com>
2021-07-16 00:13:04 -0300

    removing file log

--- src/core/log/index.ts
+++ src/core/log/index.ts
@@ -1,28 +1,13 @@
 import { createLogger, format, transports } from "winston";
 
-const defaultFormats = format.combine(
-  format.errors({ stack: true }),
-  format.timestamp({
-    format: "YYYY-MM-DD HH:mm:ss Z",
-  }),
-  format.splat(),
-  format.json(),
-  format.prettyPrint()
-)
-
 export const logger = createLogger({
   transports: [
-    new transports.File({
-      filename: "log",
-      dirname: "logs",
-      zippedArchive: true,
-      format: defaultFormats,
-    }),
     new transports.Console({
       format: format.combine(
-        defaultFormats,
-        format.colorize()
-      )
+        format.prettyPrint(),
+        format.simple(),
+        format.splat()
+      ),
     }),
   ],
 });
