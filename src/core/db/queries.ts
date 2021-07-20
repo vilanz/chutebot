271beca770533e9f708b939a6b520d82c1057bb0
vilanz <vs.lanzarini@gmail.com>
2021-07-19 23:48:40 -0300

    removing unused queries

--- src/core/db/queries.ts
+++ src/core/db/queries.ts
@@ -1,16 +1,9 @@
 import { differenceInCalendarDays } from "date-fns";
 import { logger } from "../log";
 import { PlayerSpell, TriviaPlayer } from "../../trivia/types";
-import { sequelizeInstance } from "./sequelize";
 import { PlayerEntity, PlayerSpellEntity } from "./models";
 
 // TODO move all this cruft to the new better-sqlite3 architecture
-
-export const getRandomPlayerId = () =>
-  PlayerEntity.findOne({
-    attributes: ["transfermarktId"],
-    order: sequelizeInstance.random(),
-  }).then((p) => p?.transfermarktId);
 
 export const getPlayerByTransfermarktId = (transfermarktId: number) =>
   PlayerEntity.findOne({
@@ -19,24 +12,6 @@ export const getPlayerByTransfermarktId = (transfermarktId: number) =>
     },
     include: [PlayerEntity.associations.spells],
   });
-
-export const getPlayerCount = (): Promise<number> => PlayerEntity.count();
-
-export const playerExists = async (
-  transfermarktId: number
-): Promise<boolean> => {
-  const count = await PlayerEntity.count({
-    where: {
-      transfermarktId,
-    },
-  });
-  return count > 0;
-};
-
-export const hasPlayers = async (): Promise<boolean> => {
-  const count = await PlayerEntity.count();
-  return count > 0;
-};
 
 export const removeOldPlayerSpells = async (
   transfermarktId: number
