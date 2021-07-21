6d68efc093ba90612d29419652c560a34fef6dc7
vilanz <vs.lanzarini@gmail.com>
2021-07-20 22:20:13 -0300

    changing db folder location

--- src/core/db/better-sqlite3.ts
+++ src/core/db/better-sqlite3.ts
@@ -1,5 +1,5 @@
 import betterSqlite3 from "better-sqlite3";
 
-export const db = betterSqlite3("./db.sqlite");
+export const db = betterSqlite3("./db/db.sqlite");
 db.pragma("synchronous = NORMAL");
 db.pragma("journal_mode = WAL");
--- src/core/db/sequelize.ts
+++ src/core/db/sequelize.ts
@@ -3,6 +3,6 @@ import { Sequelize } from "sequelize";
 
 export const sequelizeInstance = new Sequelize({
   dialect: "sqlite",
-  storage: path.resolve("./db.sqlite"),
+  storage: path.resolve("./db/db.sqlite"),
   logging: false,
 });
